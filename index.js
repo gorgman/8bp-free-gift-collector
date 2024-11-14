b792165a5e380f8f3d3dd2f3ceef9b2200463281
Aditeya Srivastava <this.is.aditeya@gmail.com>
2024-11-14 11:22:26 -0500

    updated script for new UI

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ import puppeteer from "puppeteer";
 import fs from "fs/promises";
 import "path";
 
+const URL = "https://8ballpool.com/en/shop";
 const USER_UNIQUE_ID = "4722012226";
 const DELAY = 100;
+
+const LOGIN = {
+  BUTTON: 'button[data-testid="btn-login-modal"]',
+  INPUT: 'input[data-testid="input-unique-id"]',
+  GO: 'button[data-testid="btn-user-go"]',
+};
 
 const makeRewardData = (imageSrc, name, quantity) => {
   return `<img src="${imageSrc}" height="25" alt="${name}"/> ${name} X ${quantity}`;
@@ -15,24 +22,20 @@ const makeRewardData = (imageSrc, name, quantity) => {
   let rewards = [];
 
   console.log("Navigating to 8 Ball Pool shop...");
-  await page.goto("https://8ballpool.com/en/shop");
-  const loginButton = await page.waitForSelector(
-    `button[data-testid="btn-login-modal"]`
-  );
+  await page.goto(URL);
+  const loginButton = await page.waitForSelector(LOGIN.BUTTON);
   if (loginButton) {
     console.log("Logging in user...");
     await loginButton.click();
-    await page.type('input[data-testid="input-unique-id"]', USER_UNIQUE_ID, {
-      delay: DELAY,
-    });
-    const goButton = await page.waitForSelector(
-      'button[data-testid="btn-user-go"]'
-    );
+    await page.type(LOGIN.INPUT, USER_UNIQUE_ID, { delay: DELAY });
+    const goButton = await page.waitForSelector(LOGIN.GO);
     await goButton.click();
   } else {
     console.log("Login button not found");
   }
-  const productCards = await page.$$(".product-list-item > .face-desktop");
+
+  const productCards = await page.$$(".product-list-item");
+  console.log(`${productCards.length} products found.`);
   for (const card of productCards) {
     const priceButton = await card.$("button");
     const price = await priceButton.evaluate((el) => el.textContent.trim());
@@ -44,16 +47,17 @@ const makeRewardData = (imageSrc, name, quantity) => {
       );
       const nameElement = await card.$("h3");
       const name = await nameElement.evaluate((el) => el.textContent.trim());
-      const quantityElement = await card.$(".text-gold");
+      const quantityElement = await card.$(".amount-text");
       const quantity = await quantityElement.evaluate((el) =>
         el.textContent.trim()
       );
-      console.log("Claimed:", name);
+      console.log(`Claimed: ${name} ${quantity} ${imageSrc}`);
       rewards.push(makeRewardData(imageSrc, name, quantity));
     }
   }
   await browser.close();
   console.log("Browser closed.");
+
   if (rewards.length !== 0) {
     const today = new Date();
     const todaysRewards = `| ${today.toLocaleDateString()} | ${rewards.join(
