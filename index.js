169d0e39f13de71dc5e7ea476a7bba4f68174663
Aditeya Srivastava <this.is.aditeya@gmail.com>
2024-11-14 13:29:23 -0500

    error when no rewards

--- index.js
+++ index.js
@@ -1,86 +1,102 @@
 import puppeteer from "puppeteer";
 import fs from "fs/promises";
-import "path";
+import path from "path";
 
-const URL = "https://8ballpool.com/en/shop";
-const USER_UNIQUE_ID = "4722012226";
-const DELAY = 100;
-
-const LOGIN = {
-  BUTTON: 'button[data-testid="btn-login-modal"]',
-  INPUT: 'input[data-testid="input-unique-id"]',
-  GO: 'button[data-testid="btn-user-go"]',
+const logMessage = (type, message) => {
+  const colors = {
+    success: "\x1b[32m",
+    error: "\x1b[31m",
+    info: "\x1b[36m",
+    reset: "\x1b[0m",
+  };
+  const color = colors[type] || colors.info;
+  console.log(`${color}%s${colors.reset}`, message, "\n");
 };
 
 const makeRewardData = (imageSrc, name, quantity) => {
   return `<img src="${imageSrc}" height="25" alt="${name}"/> ${name} X ${quantity}`;
 };
 
-(async () => {
+const URL = "https://8ballpool.com/en/shop";
+const USER_UNIQUE_ID = "4722012226";
+const DELAY = 100;
+
+const collectRewards = async () => {
   const browser = await puppeteer.launch({ headless: true, slowMo: DELAY });
   const page = await browser.newPage();
-  let rewards = [];
-
-  console.log("Navigating to 8 Ball Pool shop...");
+  logMessage("info", `🌐 Navigating to ${URL}`);
   await page.goto(URL);
-  const loginButton = await page.waitForSelector(LOGIN.BUTTON);
+  const loginButton = await page.waitForSelector(
+    'button[data-testid="btn-login-modal"]'
+  );
   if (loginButton) {
-    console.log("Logging in user...");
     await loginButton.click();
-    await page.type(LOGIN.INPUT, USER_UNIQUE_ID, { delay: DELAY });
-    const goButton = await page.waitForSelector(LOGIN.GO);
+    await page.type('input[data-testid="input-unique-id"]', USER_UNIQUE_ID, {
+      delay: DELAY,
+    });
+    const goButton = await page.waitForSelector(
+      'button[data-testid="btn-user-go"]'
+    );
     await goButton.click();
+    logMessage("success", "✅ User logged in.");
   } else {
-    console.log("Login button not found");
+    throw new Error("Unable to login.");
   }
-
-  const productCards = await page.$$(".product-list-item");
-  console.log(`${productCards.length} products found.`);
-  for (const card of productCards) {
-    const priceButton = await card.$("button");
+  let rewards = [];
+  const products = await page.$$(".product-list-item");
+  logMessage("info", `💡 ${products.length} products found.`);
+  for (const product of products) {
+    const priceButton = await product.$("button");
     const price = await priceButton.evaluate((el) => el.textContent.trim());
     if (price === "FREE") {
       await priceButton.click();
-      const imageElement = await card.$("img");
+      const imageElement = await product.$("img");
       const imageSrc = await imageElement.evaluate((i) =>
         i.getAttribute("src")
       );
-      const nameElement = await card.$("h3");
+      const nameElement = await product.$("h3");
       const name = await nameElement.evaluate((el) => el.textContent.trim());
-      const quantityElement = await card.$(".amount-text");
+      const quantityElement = await product.$(".amount-text");
       const quantity = await quantityElement.evaluate((el) =>
         el.textContent.trim()
       );
-      console.log(`Claimed: ${name} ${quantity} ${imageSrc}`);
       rewards.push(makeRewardData(imageSrc, name, quantity));
+      logMessage("success", `🎉 Claimed: ${name}`);
     }
   }
   await browser.close();
-  console.log("Browser closed.");
-
-  if (rewards.length !== 0) {
-    const today = new Date();
-    const todaysRewards = `| ${today.toLocaleDateString()} | ${rewards.join(
-      "; "
-    )} |\n`;
-    let prevReadmeContent;
-    if (today.getDate() === 1) {
-      const monthYear = `${String(today.getMonth() + 1).padStart(
-        2,
-        "0"
-      )}-${today.getFullYear()}`;
-      const archiveFileName = path.join("archive", `${monthYear}.md`);
-      await fs.mkdir("archive", { recursive: true });
-      const currentReadme = await fs.readFile("README.md", "utf8");
-      await fs.writeFile(archiveFileName, currentReadme);
-      console.log(`Archived ${archiveFileName}`);
-      prevReadmeContent = await fs.readFile("README.example.md", "utf8");
-    } else {
-      prevReadmeContent = await fs.readFile("README.md", "utf8");
-    }
-    prevReadmeContent += todaysRewards;
-    await fs.writeFile("README.md", prevReadmeContent);
-  } else {
-    console.log("No free gifts found");
+  logMessage("info", "❎ Browser closed.");
+  if (rewards.length === 0) {
+    throw new Error("No rewards found");
   }
-})();
+  return rewards;
+};
+
+const updateReadme = async (rewards) => {
+  const today = new Date();
+  const todaysRewards = `| ${today.toLocaleDateString()} | ${rewards.join(
+    "; "
+  )} |\n`;
+  let prevReadmeContent;
+  if (today.getDate() === 1) {
+    const monthYear = `${String(today.getMonth() + 1).padStart(
+      2,
+      "0"
+    )}-${today.getFullYear()}`;
+    const archiveFileName = path.join("archive", `${monthYear}.md`);
+    await fs.mkdir("archive", { recursive: true });
+    const currentReadme = await fs.readFile("README.md", "utf8");
+    await fs.writeFile(archiveFileName, currentReadme);
+    logMessage("info", `🗄️ Archived ${monthYear}`);
+    prevReadmeContent = await fs.readFile("README.example.md", "utf8");
+  } else {
+    prevReadmeContent = await fs.readFile("README.md", "utf8");
+  }
+  prevReadmeContent += todaysRewards;
+  await fs.writeFile("README.md", prevReadmeContent);
+  logMessage("success", `📝 Updated README`);
+};
+
+const rewards = await collectRewards();
+await updateReadme(rewards);
+logMessage("success", "🤖 Script complete.");
